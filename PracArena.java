c16c0ebf04327da760f428e7535557f04881c75c
Bibhavshah <202051212@iiitvadodara.ac.in>
2023-01-18 18:08:05 +0530

    Add new file

---
+++ PracArena.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class PracArena {
+//Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+	}
+
+}
